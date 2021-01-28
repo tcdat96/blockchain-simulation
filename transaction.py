3d6a493ab20c0c853be332df58f9bc672680da6d
tcdat96 <tcdat96@gmail.com>
2021-01-28 01:30:57 -0600

    define transaction structure

--- transaction.py
+++ transaction.py
@@ -1,51 +1,71 @@
+
 '''
 transaction.py
 create transaction for blockchain 
 '''
 import time
-from fastecdsa import ecdsa, point
 from util import *
+from crypto import *
 
+# senders list of tuple(hash of transaction, address)
+# receivers list of tuple(amount, address)
 class Transaction:
-  def __init__(self, sender, receiver, amount, timestamp=time.asctime().encode(), signature=(0,0)):  
-    self.Timestamp = timestamp #bytestring size 24
-    self.Sender = sender #tuple of 2 numbers
-    self.Receiver = receiver #tuple of 2 numbers
-    self.Amount = amount #float with maximum 12 digits after floating point
-    self.Signature = signature #tuple of 2 numbers
+  def __init__(self, T=None, senders = list(), receivers = list()):
+    if T == None:
+      self.Timestamp = time.asctime().encode()
+      self.Senders = senders
+      self.Receivers = receivers
+      self.Digest = double_sha256(self.to_bytes(nohash=True, nosign=True))
+    else:
+      self.from_bytes(T)
   
   def __str__(self):
     return "Created Time: " + self.Timestamp.decode() + \
-           "\nAmount: " + str(self.Amount) + \
-           "\nSender Address: " + str(self.Sender) + \
-           "\nReceiver Address: " + str(self.Receiver) + \
-           "\nTransaction Authetication Code: " + str(self.Signature)
+           "\nHash: " + str(self.Digest) + \
+           "\nSenders: " + str(self.Senders) + \
+           "\nReceivers: " + str(self.Receivers) +\
+           "\nSignatures: " + str(self.Signatures)
+  
+  def to_bytes(self, nosign = False, nohash=False):
+    senders = [b64encode(digest + b'\n' + address) 
+                  for digest, address in self.Senders]
 
-  def from_bytes(s):
-    timestamp = s[:24]
-    sender = b2pu(s[24:88])
-    receiver = b2pu(s[88:152])
-    amount = b2f(s[152:168])
-    signature = b2pu(s[168:])
-    return Transaction(sender, receiver, amount, timestamp, signature)
+    receivers = [b64encode(str(amount).encode() + b'\n' + address)
+                    for amount, address in self.Receivers]
 
-  def to_bytes(self):
-    s = self.Timestamp #size 24
-    s += pu2b(self.Sender) #sz 64
-    s += pu2b(self.Receiver) #sz 64
-    s += f2b(self.Amount) #sz 16
-    s += pu2b(self.Signature) #sz 64
-    return s
+    s = [self.Timestamp, 
+            b64encode(b'\n'.join(senders)),
+            b64encode(b'\n'.join(receivers))]
 
-  def sign(self, PR): 
-    m = self.to_bytes()[:-64]
-    self.Signature = ecdsa.sign(m, PR)
-    
+    if not nohash:
+      s.insert(0, self.Digest)
+
+    if not nosign:
+      s.append(b64encode(b'\n'.join(self.Signatures)))
+
+    return b'\n'.join(s)
+
+  def from_bytes(self, T):
+    T = T.splitlines()
+    self.Digest = T[0]
+    self.Timestamp = T[1]
+    self.Signatures = b64decode(T[-1]).splitlines()
+
+    senders = b64decode(T[2]).splitlines()
+    self.Senders = [b64decode(s).splitlines() for s in senders]
+
+    receivers = b64decode(T[3]).splitlines()
+    receivers = [b64decode(r).splitlines() for r in receivers]
+    self.Receivers = [[float(r[0].decode()), r[1]] for r in receivers]
+
+  def sign(self, PRs):
+    m = self.to_bytes(nosign = True)
+    self.Signatures = [sign(m, PR) for PR in PRs]
+
   def verify(self):
-    m = self.to_bytes()[:-64]
-    PU = point.Point(self.Sender[0], self.Sender[1])
-    return ecdsa.verify(self.Signature, m, PU)
+    m = self.to_bytes(nosign=True)
+    for sig, address in zip(self.Signatures, self.Senders):
+      if not verify(sig, m, address[1]):
+        return False
 
-  def digest(self):
-    m = self.to_bytes()
-    return double_sha256(m)
+    return True
